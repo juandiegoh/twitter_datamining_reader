ee307a0439d4b2c9aea550064e5f8c0ee1ff05c1
Juan Diego Hereñu <juan.herenu@mercadolibre.com>
2014-06-09 15:46:11 -0300

    StreamReader added with new flow

--- src/main/groovy/com/jherenu/tweeter_reader/Main.groovy
+++ src/main/groovy/com/jherenu/tweeter_reader/Main.groovy
@@ -3,5 +3,15 @@ package com.jherenu.tweeter_reader
 class Main {
     static main(args) {
         println 'Hello World'
+        def twitterStreamReader = new TwitterStreamReader()
+        twitterStreamReader.startConsumer()
+
+        while(twitterStreamReader.keepReading()) {
+            sleep 1000
+        }
+
+        twitterStreamReader.stopConsumer()
+
+        println twitterStreamReader.getResult()
     }
 }
--- src/main/groovy/com/jherenu/tweeter_reader/TwitterStreamReader.groovy
+++ src/main/groovy/com/jherenu/tweeter_reader/TwitterStreamReader.groovy
@@ -4,20 +4,25 @@ import twitter4j.FilterQuery
 import twitter4j.TwitterStreamFactory
 import twitter4j.UserStreamAdapter
 
-class TweeterStreamAdapter {
+class TwitterStreamReader {
 
-    def startReading() {
-        def stream = new TwitterStreamFactory().instance
+    def MAX_TWEETS = 10
+
+    def stream
+    def tweets = []
+    def tweetsCounter = 0
+    def counter = 0
+
+    TwitterStreamReader() {
+        this.stream = new TwitterStreamFactory().instance
+    }
+
+    def startConsumer() {
         def listener = [
                 onStatus: { st ->
-                    println "Count: ${counter++}"
-                    tweetsCounter++
+                    println "Count: ${tweetsCounter++}"
                     println "Tweets Counter: $st.text"
-
-                    if(tweetsCounter > maxTweets) {
-                        stream.cleanUp()
-                        stream.shutdown()
-                    }
+                    tweets.add(st)
                 },//"$st.user.screenName: $st.text" },
                 onException: { ex -> ex.printStackTrace() },
                 onDeletionNotice: { statusDeletionNotice -> },
@@ -26,11 +31,24 @@ class TweeterStreamAdapter {
         stream.addListener(listener)
 
         FilterQuery filter = new FilterQuery();
-        String[] keywordsArray = ['banega', 'eber banega'];
+        String[] keywordsArray = ['san antonio spurs'];
         String[] languages = ['en']
         filter.language(languages)
         filter.track(keywordsArray);
 
         stream.filter(filter);
+    }
+
+    boolean keepReading() {
+        return tweets.size() < MAX_TWEETS
+    }
+
+    def stopConsumer() {
+        stream.cleanUp()
+        stream.shutdown()
+    }
+
+    def getResult() {
+        return tweets
     }
 }
